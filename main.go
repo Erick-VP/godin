9daaff31200f12a1b7856a9106a232751ccf407b
Erick-VP <erickvscletal31@gmail.com>
2026-05-14 11:54:45 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
-wpackage main
+package main
 
-import "github.com/Erick-VP/godin/router"
+import (
+	"github.com/Erick-VP/godin/config"
+	"github.com/Erick-VP/godin/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("failed to initialize config: %v", err)
+		return
+	}
 
 	router.Initialize()
 }
